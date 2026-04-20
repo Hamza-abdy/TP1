902f2d2d91a878449a61b5f9c1521b3eafed6f64
Hamza ABDY <abdyhamza97@gmail.com>
2026-04-20 11:38:32 +0200

    Exercice 2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Bonjour, je m'appelle Hamza !");
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int age;
+        string nom;
+        double salaire;
+        bool estEtudiant;
+
+        Console.Write("Entrez votre nom : ");
+        nom = Console.ReadLine();
+
+        Console.Write("Entrez votre âge : ");
+        age = int.Parse(Console.ReadLine());
+
+        salaire = 50000;
+        estEtudiant = false;
+
+        Console.WriteLine($"Bonjour {nom}, vous avez {age} ans.");
+        Console.WriteLine($"Salaire : {salaire} MRU");
+        Console.WriteLine($"Etudiant : {estEtudiant}");
+    }
+}
